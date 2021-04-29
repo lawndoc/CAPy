c58b6245583644564129330bc35967e354961baf
C.J. May <doctormay6@gmail.com>
2021-04-29 00:33:05 -0500

    added ownca module

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,9 @@
 from config import Config
 from flask import Flask
+from ownca import CertificateAuthority
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+ca = CertificateAuthority(ca_storage=Config.CA_CERT_DIR, common_name=Config.CA_NAME)
