029947aae3a3a396fcea57591557889cb7f4e0b3
C.J. May <doctormay6@gmail.com>
2021-04-29 00:33:52 -0500

    removed cert directories and added CA name

--- config.py
+++ config.py
@@ -4,5 +4,4 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "super-secret-default-key"
     CA_CERT_DIR = os.environ.get("CA_CERT_DIR") or "/opt/CAPy/CA/"
-    HOST_CERT_DIR = os.environ.get("HOST_CERT_DIR") or "/opt/CAPy/host/"
-    CLIENT_CERT_DIR = os.environ.get("CLIENT_CERT_DIR") or "/opt/CAPy/client/"
+    CA_NAME = os.environ.get("CA_NAME") or "CAPy Root CA"
