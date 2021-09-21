28431a8cc6e5159b70630acc425e53c162ec9551
C.J. May <doctormay6@gmail.com>
2021-09-21 14:47:30 -0500

    remove extra '/' after directory

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "super-secret-default-key"
-    CA_CERT_DIR = os.environ.get("CA_CERT_DIR") or ("/opt/CAPy/CA/" if os.name != "nt" else "C:\\Users\\Public\\CAPy\\CA\\")
+    CA_CERT_DIR = os.environ.get("CA_CERT_DIR") or ("/opt/CAPy/CA" if os.name != "nt" else "C:\\Users\\Public\\CAPy\\CA")
     CA_NAME = os.environ.get("CA_NAME") or "CAPy Root CA"
