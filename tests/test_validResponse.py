e4bbe853d568853b15d0686da44504db8f8b781d
C.J. May <doctormay6@gmail.com>
2021-09-22 19:12:39 -0500

    renamed test

---
+++ tests/test_validResponse.py
@@ -0,0 +1,24 @@
+from api import app
+import json
+import pytest
+
+@pytest.fixture
+def client():
+    app.testing = True
+    yield app.test_client()
+
+def testRootCert(client):
+    response = client.get("/ca/root-trust")
+    assert response.status_code == 200
+    assert "root" in response.json
+
+
+def testHostCert(client):
+    response = client.get("/ca/host-certificate",
+                          headers={"Content-Type": "application/json"},
+                          data=json.dumps({"hostname": "testhost.local"}))
+    assert response.status_code == 200
+    assert "key" in response.json
+    assert "cert" in response.json
+    assert "root" in response.json
+
