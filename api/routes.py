ff15ac857985359a5536c7174a6e520350ec1ee3
C.J. May <doctormay6@gmail.com>
2021-11-15 14:24:41 -0600

    added get request and changed new cert to post

--- api/routes.py
+++ api/routes.py
@@ -1,6 +1,7 @@
 from api import app, ca
 from flask import request
 from ownca import utils
+from ownca.exceptions import OwnCAInvalidCertificate
 
 
 @app.route("/ca/root-trust", methods=["GET"])
@@ -10,46 +11,68 @@ def rootTrust():
     return {"root": cert}
 
 
-@app.route("/ca/host-certificate", methods=["GET"])
-def hostCertificate():
+@app.get("/ca/host-certificate")
+def getHostCertificate():
+    """ Get and return an existing host certificate """
+    try:
+        certHostname = request.json["hostname"]
+        if not utils.validate_hostname(certHostname):
+            raise ValueError
+    except KeyError:
+        return {"error": "hostname not found in the request"}, 400
+    except ValueError:
+        return {"error": "hostname in the request was not standard FQDN"}, 400
+    except TypeError:
+        return {"error": "please ensure Content-Type is application/json"}, 400
+    try:
+        serverCert = ca.load_certificate(request.json["hostname"])
+    except OwnCAInvalidCertificate:
+        return {"error": "the certificate does not exist"}, 400
+    key = serverCert.key_bytes.decode()
+    cert = serverCert.cert_bytes.decode()
+    rootCert = ca.cert_bytes.decode()
+    return {"key": key, "cert": cert, "root": rootCert}
+
+
+@app.post("/ca/host-certificate")
+def newHostCertificate():
     """ Generate and return a new host certificate """
-    if request.method == "GET":
-        try:
-            certHostname = request.json["hostname"]
-            if not utils.validate_hostname(certHostname):
-                raise ValueError
-        except KeyError:
-            return {"error": "hostname not found in the request"}
-        except ValueError:
-            return {"error": f"hostname in the request was not standard FQDN --> '{certHostname}'"}
-        except TypeError:
-            return {"error": "please ensure Content-Type is application/json"}
-        certMaxDays = request.json.get("maxDays", 825)
-        certCommonName = request.json.get("CN", "")
-        certDnsNames = request.json.get("DNS", [])
-        certOids = {"country_name": request.json.get("C", ""),
-                    "state_or_province": request.json.get("ST", ""),
-                    "locality_name": request.json.get("L", ""),
-                    "street_address": request.json.get("A", ""),
-                    "organization_name": request.json.get("O", ""),
-                    "organization_unit_name": request.json.get("OU", ""),
-                    "email_address": request.json.get("E", "")}
-        # remove blank OIDs to make ownca happy
-        for oid in list(certOids.keys()):
-            if not certOids[oid]:
-                del certOids[oid]
-        certPublicExponent = request.json.get("publicExponent", 65537)
-        certKeySize = request.json.get("keySize", 2048)
-        serverCert = ca.issue_certificate(hostname=certHostname,
-                                          maximum_days=certMaxDays,
-                                          common_name=certCommonName,
-                                          dns_names=certDnsNames,
-                                          oids=certOids,
-                                          public_exponent=certPublicExponent,
-                                          key_size=certKeySize)
-        key = serverCert.key_bytes.decode()
-        cert = serverCert.cert_bytes.decode()
-        rootCert = ca.cert_bytes.decode()
-        return {"key": key, "cert": cert, "root": rootCert}
+    try:
+        certHostname = request.json["hostname"]
+        if not utils.validate_hostname(certHostname):
+            raise ValueError
+    except KeyError:
+        return {"error": "hostname not found in the request"}
+    except ValueError:
+        return {"error": f"hostname in the request was not standard FQDN --> '{certHostname}'"}
+    except TypeError:
+        return {"error": "please ensure Content-Type is application/json"}
+    certMaxDays = request.json.get("maxDays", 825)
+    certCommonName = request.json.get("CN", request.json["hostname"])
+    certDnsNames = request.json.get("DNS", [])
+    certOids = {"country_name": request.json.get("C", ""),
+                "state_or_province": request.json.get("ST", ""),
+                "locality_name": request.json.get("L", ""),
+                "street_address": request.json.get("A", ""),
+                "organization_name": request.json.get("O", ""),
+                "organization_unit_name": request.json.get("OU", ""),
+                "email_address": request.json.get("E", "")}
+    # remove blank OIDs to make ownca happy
+    for oid in list(certOids.keys()):
+        if not certOids[oid]:
+            del certOids[oid]
+    certPublicExponent = request.json.get("publicExponent", 65537)
+    certKeySize = request.json.get("keySize", 2048)
+    serverCert = ca.issue_certificate(hostname=certHostname,
+                                        maximum_days=certMaxDays,
+                                        common_name=certCommonName,
+                                        dns_names=certDnsNames,
+                                        oids=certOids,
+                                        public_exponent=certPublicExponent,
+                                        key_size=certKeySize)
+    key = serverCert.key_bytes.decode()
+    cert = serverCert.cert_bytes.decode()
+    rootCert = ca.cert_bytes.decode()
+    return {"key": key, "cert": cert, "root": rootCert}
 
 # TODO: implement PEM certificate revocation
--- tests/test_validResponse.py
+++ tests/test_validResponse.py
@@ -7,6 +7,14 @@ def testRootTrustResponse(client):
     assert "root" in response.json
 
 def testHostCertResponse(client):
+    response = client.post("/ca/host-certificate",
+                          headers={"Content-Type": "application/json"},
+                          data=json.dumps({"hostname": "testhost.local"}))
+    assert response.status_code == 200
+    assert "key" in response.json
+    assert "cert" in response.json
+    assert "root" in response.json
+    # test GET now that cert is created
     response = client.get("/ca/host-certificate",
                           headers={"Content-Type": "application/json"},
                           data=json.dumps({"hostname": "testhost.local"}))
