d87789d70dd23b006174472d8c450aecd9ddb39c
C.J. May <doctormay6@gmail.com>
2022-06-23 13:36:52 -0500

    add venv to path

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+# add virtualenv to PATH
+export VIRTUAL_ENV=/home/abc/venv
+export PATH="/home/abc/venv/bin:$PATH"
+
 # initialize CA files before multithreading
 python3.9 init_CAPy.py --proxy ${PROXY_DOMAIN}
 
 # start async flask service
-gunicorn -b 0.0.0.0:5000 -w 4 -k gevent --worker-tmp-dir /dev/shm api:app
+gunicorn -b 0.0.0.0:5000 -w 2 -k gevent --worker-tmp-dir /dev/shm api:app
